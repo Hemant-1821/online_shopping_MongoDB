97f36eba0d6f007b4425c92d76f22ffd2084fcab
Hemant Singh <singh.hemant9583@gmail.com>
2020-07-07 20:14:42 +0530

    Sessions and cookies

--- app.js
+++ app.js
@@ -3,23 +3,43 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Hemant:ovDzbvDxBhVe8Jnt@cluster0.jcml6.mongodb.net/shop2';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store 
+  })
+); 
 
 app.use((req, res, next) => {
-  User.findById("5eec7649214d9c1150c6c6b8")
+  if (!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next(); 
@@ -29,13 +49,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://Hemant:ovDzbvDxBhVe8Jnt@cluster0.jcml6.mongodb.net/shop2?retryWrites=true'
-  )
+  .connect(MONGODB_URI)
   .then(result =>{
     User.findOne().then(user => {
       if (!user) {
